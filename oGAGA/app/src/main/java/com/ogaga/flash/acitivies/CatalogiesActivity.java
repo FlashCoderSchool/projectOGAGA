18f552ba6bb387587ccd56c9081db5c622ebf3e2
Nam Le <nam.le@digital-identity.com>
2016-04-19 14:39:26 +0700

    connect to user Registration, Product Sell

--- oGAGA/app/src/main/java/com/ogaga/flash/acitivies/CatalogiesActivity.java
+++ oGAGA/app/src/main/java/com/ogaga/flash/acitivies/CatalogiesActivity.java
@@ -1,29 +1,33 @@
 package com.ogaga.flash.acitivies;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.firebase.client.ChildEventListener;
-import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
 import com.firebase.ui.FirebaseListAdapter;
-import com.firebase.ui.FirebaseRecyclerAdapter;
 import com.ogaga.flash.R;
 import com.ogaga.flash.clients.FirebaseClient;
 import com.ogaga.flash.clients.ImgurClient;
@@ -39,36 +43,66 @@ import java.io.IOException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 public class CatalogiesActivity extends AppCompatActivity {
-    FirebaseListAdapter<Catalogies> firebaseAdapter;
-    Firebase firebase;
+
+
     // PICK_PHOTO_CODE is a constant integer
     public final static int PICK_PHOTO_CODE = 1046;
+    FirebaseListAdapter<Catalogies> firebaseAdapter;
+    Firebase firebase;
+    //@Bind(R.id.rvCate)
+    //RecyclerView rvCate;
+    // private ArrayList<Catalogies> cateList;
+    // private CategoryAdapter cateAdapter;
+    @Bind(R.id.lvCatalogies)
+    ListView lvCatalogies;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.drawer_layout)
+    DrawerLayout mDrawer;
+    @Bind(R.id.nvView)
+    NavigationView nvDrawer;
+    private ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalogies);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        drawerToggle = setupDrawerToggle();
+        // Tie DrawerLayout events to the ActionBarToggle
+        nvDrawer.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        //selectDrawerItem(menuItem);
+                        switch (menuItem.getItemId()) {
+                            case R.id.navSetting:
+                                return true;
+                            case R.id.navUserProfile:
+                                Intent intent = new Intent(CatalogiesActivity.this, UserRegistryActivity.class);
+                                startActivity(intent);
+
+                        }
+                        return true;
+                    }
+                });
+
+
         Firebase.setAndroidContext(this);
-        firebase=FirebaseClient.getCatalogies();
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        firebase = FirebaseClient.getCatalogies();
+        Button fab = (Button) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //onPickPhoto(view);
-                Intent intent=new Intent(CatalogiesActivity.this,UserRegistryActivity.class);
+                Intent intent = new Intent(CatalogiesActivity.this, SellActivity.class);
                 startActivity(intent);
-
             }
         });
         popularView();
-
     }
 
     @Override
@@ -77,18 +111,39 @@ public class CatalogiesActivity extends AppCompatActivity {
         firebaseAdapter.cleanup();
     }
 
-    public void popularView(){
+    public void popularView() {
+
         firebaseAdapter = new FirebaseListAdapter<Catalogies>(this, Catalogies.class,
                 R.layout.catalogies_item, firebase) {
             @Override
             protected void populateView(View v, Catalogies model, int position) {
-                ImageView ivPhoto=(ImageView) v.findViewById(R.id.ivPicture);
+                ImageView ivPhoto = (ImageView) v.findViewById(R.id.ivPicture);
                 Picasso.with(getApplicationContext()).load(model.getUrl()).into(ivPhoto);
-                ((TextView)v.findViewById(R.id.tvName)).setText(model.getName());
+                ((TextView) v.findViewById(R.id.tvName)).setText(model.getName());
             }
 
         };
+        //get category list
+        //getItems();
+        //rvCate.setHasFixedSize(true);
+        // ListView
+        // rvCate.setLayoutManager(new LinearLayoutManager(this));
+        // create an Object for Adapter
+        //cateAdapter = new CategoryAdapter(cateList);
+        //rvCate.setAdapter(cateAdapter);*/
+        lvCatalogies.setAdapter(firebaseAdapter);
     }
+    /*public void  getItems(){
+        cateList = new ArrayList<>();
+        final String[] cates = {"Organic fruit", "Vegetables", "Seafood"};
+        final int[] images = {R.drawable.fruit,R.drawable.vegetable,R.drawable.fish};
+        for (int i = 0; i < cates.length; i++) {
+            Catalogies cate = new Catalogies(cates[i],images[i]);
+            cateList.add(cate);
+        }
+    }*/
+
+
     public void onPickPhoto(View view) {
         // Create intent for picking a photo from the gallery
         Intent intent = new Intent(Intent.ACTION_PICK,
@@ -101,26 +156,30 @@ public class CatalogiesActivity extends AppCompatActivity {
             startActivityForResult(intent, R.integer.PICK_PHOTO_CODE);
         }
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_catalogies, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-      /*  if (id == R.id.action_settings) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
             return true;
-        }*/
-
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    // `onPostCreate` called when activity start-up is complete after `onStart()`
+    // NOTE! Make sure to override the method with only a single `Bundle` argument
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -128,7 +187,7 @@ public class CatalogiesActivity extends AppCompatActivity {
         if (requestCode == R.integer.PICK_PHOTO_CODE) {
             if (resultCode == RESULT_OK) {
                 if (data != null) {
-                    try{
+                    try {
                         Uri photoUri = data.getData();
                         // Do something with the photo based on Uri
                         Bitmap selectedImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoUri);
@@ -144,7 +203,7 @@ public class CatalogiesActivity extends AppCompatActivity {
                                 firebase.push().setValue(catalogies);
                             }
                         }).Execute(upload, new UiCallback());
-                    }catch (IOException e){
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
@@ -153,6 +212,11 @@ public class CatalogiesActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    /*Functions*/
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open, R.string.drawer_close);
     }
 
 }
